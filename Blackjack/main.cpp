481f79687e84da2d12078cd674c919341b30d5ff
Bradyn Glines <glinesbdev@gmail.com>
2022-08-04 17:00:34 -0400

    Game waits to exit so player can see stats

--- Blackjack/main.cpp
+++ Blackjack/main.cpp
@@ -1,13 +1,18 @@
 #include "Blackjack.h"
 #include <format>
 #include <iostream>
+#include <limits>
 
 int main()
 {
 	Blackjack game{};
 	Blackjack::GameStats stats{ game.play() };
 
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	std::cout << std::format("\n***** Game Stats *****\nGames won: {}\nGames tied: {}\nGames lost: {}\n", stats.games_won, stats.games_tied, stats.games_lost);
+	std::cout << "\nPress Enter to exit...";
+	std::cin.get();
 
 	return 0;
 }
